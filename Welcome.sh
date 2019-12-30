3b8338a1e16e416966aa11f707cbeb154e737035
Mr-004 <57210483+Mr-004@users.noreply.github.com>
2019-12-30 17:09:45 +0700

    Add files via upload

---
+++ Welcome.sh
@@ -0,0 +1,24 @@
+clear 
+python2 meizu.py
+clear
+blue='\033[34;1m'
+green='\033[32;1m'                                        
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+sleep 1
+toilet -f big -F gay "T00Ls" | lolcat
+toilet -f big -F gay "Mr.004V5" 
+echo ""      
+echo "\033[32;1mAuthor : Mr.004 Feat Beni_Hat12,Mr.003,Mr.006" 
+echo "\033[35;1mTeam : 004 CYEBER TEAM"
+echo "\033[33;1mKontak Me :  081228244243"
+echo "\033[31;1mSubcribe Youtube:"
+echo "\033[33;1mGamer 4 LIve" "\033[32;1m&" "\033[36;1mKimi Hime"
+echo "\033[34;1m[============================[>"
+sleep 1
+echo ""
+echo "\033[32;1mGunakan Dengan Bijak Aku Hanyalah Newbie Mr.004?:"
+echo "\033[36;1m"
